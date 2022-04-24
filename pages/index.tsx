b899bb6dad00607b694ab9835b0f2d1868dbf069
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2022-04-24 00:21:00 -0400

    add cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import React from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  React.useEffect(() => () => {console.log('cleanup')}, [])
+
   return (
     <div className={styles.container}>
       <Head>
